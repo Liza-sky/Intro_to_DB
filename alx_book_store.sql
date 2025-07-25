db971043f4cf7434d641870e25886de77d6713db
Liza-sky <119234355+Liza-sky@users.noreply.github.com>
2025-07-26 01:30:09 +0300

    updated file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -33,7 +33,7 @@ CREATE TABLE Books (
     author_id INT NOT NULL,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 )
 """
 mycursor.execute(sql)
@@ -56,7 +56,7 @@ CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 )
 """
 mycursor.execute(sql)
@@ -69,8 +69,8 @@ CREATE TABLE Order_Details (
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 )
 """
 mycursor.execute(sql)
