d8fc6c08f68d26014ec7e56fa96b4b6123f737b8
Liza-sky <119234355+Liza-sky@users.noreply.github.com>
2025-07-26 02:07:59 +0300

    update task_3.sql file

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,22 @@
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="123456",
+    database="yourdatabase"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("USE alx_book_store")
+
+sql = """
+Show Tables;
+"""
+mycursor.execute(sql)
+tables = mycursor.fetchall()
+
+
+
 
