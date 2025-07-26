25bf8c5372009b1eb0d07e1fdaa6f72d6cd173f8
Liza-sky <119234355+Liza-sky@users.noreply.github.com>
2025-07-26 03:57:51 +0300

    update

--- task_5.sql
+++ task_5.sql
@@ -10,11 +10,8 @@ try:
 
     mycursor = mydb.cursor()
 
-USE alx_book_store;
-mycursor.execute("USE alx_book_store")
-
-
- "INSERT INTO Customer (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')"
+# Remove redundant USE statement and assign SQL to a variable
+sql = "INSERT INTO Customer (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')"
 mycursor.execute(sql)
 mydb.commit()
  
