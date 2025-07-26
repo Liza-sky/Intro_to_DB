4ee70b58487796daa9f76fc7eb36a136f91fd53e
Liza-sky <119234355+Liza-sky@users.noreply.github.com>
2025-07-26 03:22:36 +0300

    another update on file

--- task_5.sql
+++ task_5.sql
@@ -11,8 +11,8 @@ import mysql.connector
     mycursor = mydb.cursor()
 
 
-sql = "INSERT INTO Customer (customer_id, customer_name, email, address)"
-val = (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+sql = "INSERT INTO Customer (customer_id, customer_name, email, address) VALUES (%s, %s, %s, %s)"
+val = (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
 mycursor.execute(sql, val)
 
 
