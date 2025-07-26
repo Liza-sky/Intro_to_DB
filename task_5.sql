d693d2a3fe78a3a074120c752a74c5477ba6394e
Liza-sky <119234355+Liza-sky@users.noreply.github.com>
2025-07-26 03:19:24 +0300

    update

--- task_5.sql
+++ task_5.sql
@@ -5,17 +5,26 @@ import mysql.connector
         host="localhost",
         user="root",
         password="123456",
-        database="yourdatabase"
+        database="alx_book_store"
 )
 
     mycursor = mydb.cursor()
-
-USE alx_book_store;
-mycursor.execute("USE alx_book_store")
 
 
 sql = "INSERT INTO Customer (customer_id, customer_name, email, address)"
 val = (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 mycursor.execute(sql, val)
-mydb.commit()  # Commit the changes
+
+
+mydb.commit()
+ print("Customer inserted successfully.")
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+
+finally:
+    if 'mycursor' in locals():
+        mycursor.close()
+    if 'mydb' in locals() and mydb.is_connected():
+        mydb.close()
 
