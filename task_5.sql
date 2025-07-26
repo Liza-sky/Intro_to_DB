529504527a0fa7bdc0f62b28c0540333e374daac
Liza-sky <119234355+Liza-sky@users.noreply.github.com>
2025-07-26 03:41:47 +0300

    update

--- task_5.sql
+++ task_5.sql
@@ -14,9 +14,8 @@ USE alx_book_store;
 mycursor.execute("USE alx_book_store")
 
 
-sql = "INSERT INTO Customer (customer_id, customer_name, email, address) VALUES (%s, %s, %s, %s)"
-val = (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
-mycursor.execute(sql, val)
+sql = "INSERT INTO Customer (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')"
+mycursor.execute(sql)
 
 
 mydb.commit()
