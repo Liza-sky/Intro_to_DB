1f695c64fd0103516a5b58c1e7cf61ca58afa391
Liza-sky <119234355+Liza-sky@users.noreply.github.com>
2025-07-26 03:08:13 +0300

    update  file

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,17 @@
+import mysql.connector
 
+
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="123456",
+        database="yourdatabase"
+)
+
+    mycursor = mydb.cursor()
+
+USE alx_book_store;
+mycursor.execute("USE alx_book_store")
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
