c538849760a156a1d4fa5bbf5c6bbdbd5042ed45
Liza-sky <119234355+Liza-sky@users.noreply.github.com>
2025-07-26 03:03:27 +0300

    update

--- task_4.sql
+++ task_4.sql
@@ -13,22 +13,6 @@ import mysql.connector
 USE alx_book_store;
 mycursor.execute("USE alx_book_store")
 
-sql = "SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA"
-val = ("John", "john@example.com")
-mycursor.execute(sql, val)
-mydb.commit()  # Commit the changes
-
-  mycursor.execute("SELECT * FROM INFORMATION_SCHEMA.COLUMNS")
-  myresult = mycursor.fetchall()
-
-  for row in myresult:
-  print(row)
-
-  sql = "UPDATE TABLE_NAME = 'Books' AND TABLE_SCHEMA = 'alx_books_store';
-  val = ("Jane", 1)
-  mycursor.execute(sql, val)
-  mydb.commit() # Commit the changes
-
-
-
-
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books' AND TABLE_SCHEMA = 'alx_book_store';
